5844b80bf75589a28d06052ca3bc2b5de4552df3
ShivamGupta7700 <shivam.pcmsi@gmail.com>
2026-06-01 10:19:52 +0530

    Refactor code structure for improved readability and maintainability

---
+++ basics/FirstLastName.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+int main(){
+
+    std:: cout << "Shivam\nGupta";
+
+    return 0;
+}
---
+++ basics/code.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // std:: cout << "hello World" << "\n"; 
+    // // \n is much faster than endl
+    
+    // int marks = 9; // 4 bytes
+    
+    // char name = 'Shivam';  //1 byte ASCII
+    
+    // float PI = 3.14f ; //4 bytes
+    
+    // bool a = true ; //1 byte
+
+    // double x = 3.9999 ; // 8 bytes
+    // int z = (int)x;
+    // cout << z << endl;
+    // cout << sizeof(marks) <<"\n";
+    
+    int a  =  3 ;
+    int b = a++;
+    cout << (a+b) << "\n";
+    
+    
+    return 0;
+}
+

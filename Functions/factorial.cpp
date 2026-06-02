fed57d29bc07278d54973ab67b6576ee1aecc7ca
ShivamGupta7700 <shivam.pcmsi@gmail.com>
2026-06-02 13:05:20 +0530

    Implement digitSum function and update main for testing

--- Functions/factorial.cpp
+++ Functions/factorial.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+int digitSum(int number){
+    int sum = 0 ;
+    while (number > 0)
+    {
+        int lastDigit = number % 10;
+        number = number / 10;
+        sum += lastDigit;
+    }
+    return sum;
+    
+}
+
 int factorial(int n){
     // if (n == 1)
     // {
@@ -11,6 +23,7 @@ int factorial(int n){
     return (n==1 ? 1 : factorial(n-1)*n);
 }
 int main(){
+    std:: cout << digitSum(143) << '\n';
     std:: cout << factorial(7) << '\n';
     
     return 0;

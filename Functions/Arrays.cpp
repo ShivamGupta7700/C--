04646664c93bd20740c0487a126aac37cb3d85fe
ShivamGupta7700 <shivam.pcmsi@gmail.com>
2026-06-05 12:25:09 +0530

    Add additional loop to display user data from array

--- Functions/Arrays.cpp
+++ Functions/Arrays.cpp
@@ -8,4 +8,10 @@ int main(){
     for ( std:: string users : userData ){
         std:: cout << users << '\n';
     }
+    
+    for ( int i = 0; i < sizeof(userData) / sizeof(std::string); i++) //remember bytes
+    {
+        std:: cout << userData[i] << "\n";
+    }
+    
 }

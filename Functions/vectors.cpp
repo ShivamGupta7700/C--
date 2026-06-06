1b3c2e3f3ed7dd212d13e3c52d70fd4d9c274d50
ShivamGupta7700 <shivam.pcmsi@gmail.com>
2026-06-06 22:12:12 +0530

    Fix printVectorValues function to correctly display vector elements and improve main function structure

--- Functions/vectors.cpp
+++ Functions/vectors.cpp
@@ -2,10 +2,26 @@
 #include <vector>
 
 using namespace std;
-
+void printVectorValues(vector <int> vec){
+    for (int numbers : vec)
+    {
+        cout << vec[numbers] << " ";
+    }
+    cout << endl;
+    
+} 
 int main(){
-    vector <int> vect;
+    vector <int> vect = {1,2,3};
+    vector <int> defalutVector(5,5);
     cout << vect[0] << "\n";
+    for (int numbers : defalutVector)
+    {
+        cout << defalutVector[numbers] << " ";
+    }
+    cout << endl;
+    printVectorValues(defalutVector);
+    
+    cout << vect.size() <<endl;
     return 0;
 }
 

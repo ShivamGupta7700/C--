8e44cba33486c6418e561564fc41173606818693
ShivamGupta7700 <shivam.pcmsi@gmail.com>
2026-06-08 22:29:52 +0530

    Refactor maximumSubArray function to simplify logic and improve readability

--- Functions/vectors.cpp
+++ Functions/vectors.cpp
@@ -27,12 +27,10 @@ void maximumSubArray(int arr[], int size){
     for (int i = 0; i < size; i++)
     {
         currentSum += arr[i];
+        maxSum = max(currentSum , maxSum);
         if (currentSum <= 0 )
         {
             currentSum = 0;
-        }
-        else{
-            maxSum = max(currentSum , maxSum);
         }
         
     }

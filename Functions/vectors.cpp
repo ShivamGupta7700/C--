e9d56c9148596daaf921cdee9bc8f843d9d6519d
ShivamGupta7700 <shivam.pcmsi@gmail.com>
2026-06-08 17:13:20 +0530

    Fix printVectorValues function to correctly output vector elements and clean up main function structure

--- Functions/vectors.cpp
+++ Functions/vectors.cpp
@@ -5,23 +5,26 @@ using namespace std;
 void printVectorValues(vector <int> vec){
     for (int numbers : vec)
     {
-        cout << vec[numbers] << " ";
+        cout << numbers << " ";
     }
     cout << endl;
     
 } 
 int main(){
-    vector <int> vect = {1,2,3};
-    vector <int> defalutVector(5,5);
-    cout << vect[0] << "\n";
-    for (int numbers : defalutVector)
-    {
-        cout << defalutVector[numbers] << " ";
-    }
-    cout << endl;
-    printVectorValues(defalutVector);
+    vector <int> vect = {1, 2, 3};
+    cout << "size (initial): " << vect.size() << endl;
+    vect.push_back(9);
+    vector <int> defalutVector(5,0);
+   
+    // printVectorValues(defalutVector);
+    cout <<"size : " << vect.size() << "\n" << "After POP --->" << " "; //size of vector 
     
-    cout << vect.size() <<endl;
+    vect.pop_back();
+    printVectorValues(vect);
+    // cout << vect.front() << endl;
+    // cout << vect.back() << endl;
+    // cout << vect.at(1) << endl;
+    cout << vect.capacity() <<endl;
     return 0;
 }
 

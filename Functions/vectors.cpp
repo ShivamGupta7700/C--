a088def47902c1aceca374c470e0c2320f8720ec
ShivamGupta7700 <shivam.pcmsi@gmail.com>
2026-06-09 12:37:44 +0530

    Add pairSum function to find two numbers that sum to a target value

--- Functions/vectors.cpp
+++ Functions/vectors.cpp
@@ -37,9 +37,32 @@ void maximumSubArray(int arr[], int size){
     cout << maxSum << endl;
     
 }
+void pairSum(vector<int> vec, int target){
+    int n  = vec.size();
+    int i=0 ,j= n - 1;
+    
+    while (i<j){
+
+        int trail = vec[i]+vec[j];
+
+        if (trail < target){
+            i++;
+        }
+        else if (trail > target)
+        {
+            j--;
+        }
+        else{
+            cout << "numbers are >>>> " << vec[i] << " and " << vec[j] <<endl;
+            break;
+        }
+    }
+    
+}
+
 int main(){
-    int trailArr[] = {1,213,-1000,13,1900};
-    vector <int> vect = {1, 2, 3, 4};
+    int trailArr[] = {1,213,-1000,13,1900,1,7,2,9,8};
+    vector <int> vect = {1, 2, 3, 4, 5};
     // cout << "size (initial): " << vect.size() << endl;
     // vect.push_back(9);
     // vector <int> defalutVector(5,0);
@@ -55,7 +78,9 @@ int main(){
     // cout << vect.capacity() <<endl;
     // reverseVector(vect); 
     // printVectorValues(vect); // not changed like array for change use & (pass by refrence)
-    maximumSubArray(trailArr, sizeof(trailArr) / sizeof(int));
+    // maximumSubArray(trailArr, sizeof(trailArr) / sizeof(int));
+    pairSum(vect, 9);
+
    
     return 0;
 }
